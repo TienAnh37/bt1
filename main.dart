d4d3a0cce8ffcb63587da7fef42e51a6670ad8d0
TienAnh37 <95766000+TienAnh37@users.noreply.github.com>
2025-03-12 12:57:24 +0700

    main.dart

--- main.dart
+++ main.dart
@@ -17,33 +17,34 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Bài đầu tiên",style: TextStyle(color: Colors.white),),
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed:() {},
-            color: Colors.white
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Bài đầu tiên",style: TextStyle(color: Colors.white),),
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed:() {},
+              color: Colors.white
+          ),
+          actions:[
+            Icon(Icons.menu, size: 30,color: Colors.blue,),
+]
         ),
+        body: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://th.bing.com/th/id/OIP.OYBor-fH-EZWvR0qyuw0XwHaHa?rs=1&pid=ImgDetMain',
+                height: 250,
+                width: 250,
+                fit: BoxFit.fill,),
+              SizedBox(width: 15,),
+              Text("Phoenix")
+            ],
+          ),
 
-      ),
-      body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.network(
-          'https://th.bing.com/th/id/OIP.OYBor-fH-EZWvR0qyuw0XwHaHa?rs=1&pid=ImgDetMain',
-          height: 250,
-          width: 250,
-          fit: BoxFit.fill,),
-        SizedBox(width: 15,),
-        Text("Phoenix")
-          ],
-        ),
-
-      )
+        )
     );
 
   }
 }
-
